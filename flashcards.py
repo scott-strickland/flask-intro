060c467ab5bc2d92d473d0a105db78af25d9c337
scott-strickland <sstrickland@sycomtech.com>
2021-03-26 17:03:04 -0400

    Completed Intro section

--- flashcards.py
+++ flashcards.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,3 +8,18 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     return "Welcome to my Flash Cards application!"
+
+
+counter = 0
+
+
+@app.route("/count_views")
+def count_views():
+    global counter
+    counter += 1
+    return "This page was server " + str(counter) + " times"
+
+
+@app.route("/date")
+def date():
+    return "This page was served at " + str(datetime.now())
