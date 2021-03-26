4bf3bf9e1c8047237aa79d9f093385b343d24712
scott-strickland <sstrickland@sycomtech.com>
2021-03-26 17:33:38 -0400

    Basic Welcome with dynamic message

--- flashcards.py
+++ flashcards.py
@@ -1,25 +1,10 @@
-from datetime import datetime
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def welcome():
-    return "Welcome to my Flash Cards application!"
-
-
-counter = 0
-
-
-@app.route("/count_views")
-def count_views():
-    global counter
-    counter += 1
-    return "This page was server " + str(counter) + " times"
-
-
-@app.route("/date")
-def date():
-    return "This page was served at " + str(datetime.now())
+    return render_template(
+        "welcome.html",
+        message="Here's a message from the view.")
