0e7717b7b61fbf2f2d38d27f5deee7fa56938825
scott-strickland <sstrickland@sycomtech.com>
2021-03-26 22:41:39 -0400

    Card view with single card

--- flashcards.py
+++ flashcards.py
@@ -1,3 +1,4 @@
+from model import db
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,3 +9,9 @@ def welcome():
     return render_template(
         "welcome.html",
         message="Here's a message from the view.")
+
+
+@app.route("/card")
+def card_view():
+    card = db[0]
+    return render_template("card.html", card=card)
